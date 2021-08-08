7fa86d4200955032642cf8b6ec4276ce9c7e72ff
Tanmay Modi <54071422+tanmaymodi@users.noreply.github.com>
2021-08-08 15:07:00 +0530

    first commit

--- plagDetector/views.py
+++ plagDetector/views.py
@@ -6,5 +6,5 @@ from django.shortcuts import render , redirect
 
 
 def home(request):
-    context = {}
+    context = {'navigation': [1,2,3,4,5,6,7]}
     return render(request,'plagDetector/home.html',context)
