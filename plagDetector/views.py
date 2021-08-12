94af8c73a4f1597bdbea3d32f71ec7e8f5d234b9
Tanmay Modi <54071422+tanmaymodi@users.noreply.github.com>
2021-08-12 16:16:34 +0530

    zam

--- plagDetector/views.py
+++ plagDetector/views.py
@@ -1,7 +1,6 @@
 from django.http import JsonResponse,HttpResponse
 from django.shortcuts import render , redirect
-
-
+import nltk
 #########################################################
 
 
@@ -10,11 +9,27 @@ def home(request):
     return render(request,'plagDetector/home.html',context)
 
 
+def plag(text1,text2):
+    str1 = ''.join(text1)
+    str2 = ''.join(text2)
+    sent_text1 = str1.split('.')
+    sent_text2 = str2.split('.')
+
+    final_list = []
+    for z in sent_text1:
+        print(z)
+        for y in sent_text2:
+            if z==y:
+                final_list.append(z)
+    
+    return final_list
+
 def search(request):
     if request.method == 'POST':
         text1 = request.POST.get('text1', None)
         text2 = request.POST.get('text2', None)
-        context = {'navigation': [text1,text2,'vau','pinu','nikhil']}
+        final_list = plag(text1,text2)
+        context = {'navigation': final_list}
         return render(request,'plagDetector/home.html',context)
     else:
         return render(request,'plagDetector/home.html')
