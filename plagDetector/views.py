2e803c1db0c343f304bdeacf55e2bd53a70a02b4
Tanmay Modi <54071422+tanmaymodi@users.noreply.github.com>
2021-08-12 12:00:47 +0530

    a

--- plagDetector/views.py
+++ plagDetector/views.py
@@ -13,7 +13,7 @@ def home(request):
 def search(request):
     if request.method == 'POST':
         text1 = request.POST.get('text1', None)
-        text2 = request.POST.get('text1', None)
+        text2 = request.POST.get('text2', None)
         context = {'navigation': [text1,text2,'vau','pinu','nikhil']}
         return render(request,'plagDetector/home.html',context)
     else:
