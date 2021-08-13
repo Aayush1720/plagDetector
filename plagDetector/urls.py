ce3d9ab98ace75af454e818c5cf8e71a993cd532
Aadarsh Goyal <aadarsh.goyal11@gmail.com>
2021-08-13 20:23:43 +0530

    Fixed login/Signup issue and added document validation on file upload

--- plagDetector/urls.py
+++ plagDetector/urls.py
@@ -14,19 +14,19 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',views.home, name="home"),
+    path('', views.home, name="home"),
     # url(r'^search/', search),
     # url(r'^index/', index)
-    path('search',views.search,name="search"),
-    path('upload',views.upload, name = "upload")
+    path('search', views.search, name="search"),
+    path('upload', views.upload, name="upload")
 ]
 
-
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL,document_root = settings.MEDIA_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- plagDetector/views.py
+++ plagDetector/views.py
@@ -1,17 +1,20 @@
-from django.http import JsonResponse,HttpResponse
-from django.shortcuts import render , redirect
+from django.http import JsonResponse, HttpResponse
+from django.shortcuts import render, redirect
 import nltk
 from .forms import UploadFileForm
-from django.core.files.storage import FileSystemStorage 
+from django.core.files.storage import FileSystemStorage
+
+
 #########################################################
 
 
 def home(request):
     context = {'navigation': []}
-    return render(request,'plagDetector/home.html',context)
+    return render(request, 'plagDetector/home.html', context)
 
-#test function to compare two texts -Tanmay/Aadarsh/Aayush
-def plag(text1,text2):
+
+# test function to compare two texts -Tanmay/Aadarsh/Aayush
+def plag(text1, text2):
     str1 = ''.join(text1)
     str2 = ''.join(text2)
     sent_text1 = str1.split('.')
@@ -19,37 +22,43 @@ def plag(text1,text2):
 
     final_list = []
     for z in sent_text1:
-        #print(z)
+        # print(z)
         for y in sent_text2:
-            if z==y:
+            if z == y:
                 final_list.append(z)
-    
+
     return final_list
 
-#test function two check two input texts
+
+# test function two check two input texts
 def search(request):
     if request.method == 'POST':
         text1 = request.POST.get('text1', None)
         text2 = request.POST.get('text2', None)
-        final_list = plag(text1,text2)
+        final_list = plag(text1, text2)
         context = {'navigation': final_list}
-        return render(request,'plagDetector/home.html',context)
+        return render(request, 'plagDetector/home.html', context)
     else:
-        return render(request,'plagDetector/home.html')
+        return render(request, 'plagDetector/home.html')
 
 
-#function to upload multiples files
-#files are stored in the ./media folder of the directory
+# function to upload multiples files
+# files are stored in the ./media folder of the directory
 def upload(request):
     if request.method == 'POST':
-       for x, upload_file in enumerate(request.FILES.getlist("document")):
-           print(upload_file.name)
-           print(upload_file.size)
-           fs = FileSystemStorage()
-           fs.save(upload_file.name, upload_file,max_length=None)
-            
+        for y, file in enumerate(request.FILES.getlist("document")):
+            allowed_ext = ['txt', 'docx', 'doc']
+            if file.name.split('.')[-1] not in allowed_ext:
+                context = {'error': 'file extension not allowed, please upload a txt/doc/docx file!'}
+                return render(request, 'plagDetector/home.html', context)
+
+        for x, upload_file in enumerate(request.FILES.getlist("document")):
+            print(upload_file.name)
+            print(upload_file.size)
+            fs = FileSystemStorage()
+            fs.save(upload_file.name, upload_file, max_length=None)
+
     return render(request, 'plagDetector/home.html')
 
-
-#need to implement
-#only docx. or text files can be uploaded
+# need to implement
+# only docx. or text files can be uploaded
